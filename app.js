0cc97326569a36e97155ae5c48fef0a3ddb3310d
tan jimmy <jimmy.tan@stud.th-deg.de>
2022-07-23 23:19:11 +0200

    Final Code

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ let app = express()
 app.listen(3000)
 app.use(express.static(__dirname))
 app.get('/', (request, response) =>{
-    response.sendFile(__dirname + '/index.html')
+    response.sendFile(__dirname + '/html/index.html')
 })
 console.log("server started at http://localhost:3000")
